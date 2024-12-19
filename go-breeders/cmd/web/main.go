b1d173e5f4ee16f88372c14d84a7ca141576d2cc
Rishabh Singh <rishabhsvats@gmail.com>
2024-12-19 18:07:09 +0530

    db integration done and basic function to call db

--- go-breeders/cmd/web/main.go
+++ go-breeders/cmd/web/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/rishabhsvats/go-patterns/go-breeders/models"
 )
 
 const PORT = ":3000"
@@ -14,10 +17,13 @@ const PORT = ":3000"
 type application struct {
 	templateMap map[string]*template.Template
 	config      appConfig
+	DB          *sql.DB
+	Models      models.Models
 }
 
 type appConfig struct {
 	useCache bool
+	dsn      string
 }
 
 func main() {
@@ -26,8 +32,18 @@ func main() {
 		templateMap: make(map[string]*template.Template),
 	}
 	flag.BoolVar(&app.config.useCache, "cache", false, "use template cache")
+	flag.StringVar(&app.config.dsn, "dsn", "mariadb:myverysecretpassword@tcp(localhost:3306)/breeders?parseTime=true&tls=false", "DSN")
 	flag.Parse()
 
+	//get database
+
+	db, err := initMySQLDB(app.config.dsn)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	app.DB = db
+	app.Models = *models.New(db)
 	srv := &http.Server{
 		Addr:              PORT,
 		Handler:           app.routes(),
@@ -38,7 +54,7 @@ func main() {
 	}
 
 	fmt.Println("Starting web application on port: ", PORT)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
