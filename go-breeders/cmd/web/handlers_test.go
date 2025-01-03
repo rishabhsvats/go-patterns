0f5d8d37daffac5d2f30738e0700a71ebb749a42
Rishabh Singh <rishabhsvats@gmail.com>
2025-01-03 18:00:47 +0530

    adpater test

--- go-breeders/cmd/web/handlers_test.go
+++ go-breeders/cmd/web/handlers_test.go
@@ -26,3 +26,23 @@ func TestApplication_GetAllDogBreedsJSON(t *testing.T) {
 	}
 
 }
+
+func TestApplication_GetAllCatBreeds(t *testing.T) {
+	//create a request
+	req, _ := http.NewRequest("GET", "/api/cat-breeds", nil)
+
+	// create a response recorder
+	rr := httptest.NewRecorder()
+	// create the handler
+
+	handler := http.HandlerFunc(testApp.GetAllCatBreeds)
+
+	// serve the handler
+	handler.ServeHTTP(rr, req)
+
+	// check response
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("wrong response code; got %d wanted 200", rr.Code)
+	}
+}
--- go-breeders/cmd/web/setup_test.go
+++ go-breeders/cmd/web/setup_test.go
@@ -5,14 +5,30 @@ import (
 	"testing"
 
 	"github.com/rishabhsvats/go-patterns/go-breeders/configuration"
+	"github.com/rishabhsvats/go-patterns/go-breeders/models"
 )
 
 var testApp application
 
 func TestMain(m *testing.M) {
+
+	testBackend := &TestBackend{}
+	testAdapter := &RemoteService{Remote: testBackend}
 	testApp = application{
-		App: configuration.New(nil),
+		App:        configuration.New(nil),
+		catService: testAdapter,
 	}
 
 	os.Exit(m.Run())
+}
+
+type TestBackend struct {
+}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	breeds := []*models.CatBreed{
+		&models.CatBreed{ID: 1, Breed: "Tomcat", Details: "Details about description of Tomcat"},
+	}
+
+	return breeds, nil
 }
